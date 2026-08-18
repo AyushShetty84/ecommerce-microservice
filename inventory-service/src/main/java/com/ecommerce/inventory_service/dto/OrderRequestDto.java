53684a3a769e8713092094fbbaf200de72b01bc7
AyushShetty84 <ayushshetty79@gmail.com>
2026-08-18 16:12:31 +0530

    feat: add inventory-service

---
+++ inventory-service/src/main/java/com/ecommerce/inventory_service/dto/OrderRequestDto.java
@@ -0,0 +1,12 @@
+package com.ecommerce.inventory_service.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class OrderRequestDto {
+    private List<OrderRequestItemDto> items;
+}
+
